ea7efad5c2c5d5a0a496f23290328322159b946b
Ahmad <AAhmad@thegoldensource.com>
2026-02-09 16:51:33 +0530

    something remove

--- First.java
+++ First.java
@@ -2,6 +2,5 @@ public class First {
     public static void main (String args[]){
         System.out.println("hello this is a first java ");
       System.out.println("this is a update code");
-      System.out.println("updated code successfully");
     }
 }

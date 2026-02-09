6ef8fb70fee0cd9b3b6ec3d71c80c339e9124488
Ahmad <AAhmad@thegoldensource.com>
2026-02-09 15:54:55 +0530

    updated print message

--- First.java
+++ First.java
@@ -1,5 +1,6 @@
 public class First {
     public static void main (String args[]){
         System.out.println("hello this is a first java ");
+        System.out.println("hello  is a update  code of first java");
     }
 }
